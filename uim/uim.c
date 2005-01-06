4074405e254c368bdf00af6d0a250c48ca430dc8
ekato <ekato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-01-06 08:03:32 +0000

    * uim/uim.c (load_conf) : Fix r127 and r128 bug.

--- uim/uim.c
+++ uim/uim.c
@@ -34,7 +34,7 @@
 #include <pwd.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <stdio.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
 #include "config.h"
@@ -503,16 +503,10 @@ load_conf()
   struct passwd *pw;
   char *fn;
   long verbose, ret;
-  FILE *fp;
+  struct stat st;
 
   verbose = uim_scm_get_verbose_level();
   fn = getenv("LIBUIM_USER_SCM_FILE");
-
-  fp = fopen(fn, "r");
-  if (!fp) {
-    return -1;
-  }
-  fclose(fp);
 
   if (!fn) {
     pw = getpwuid(getuid());
@@ -520,6 +514,11 @@ load_conf()
     sprintf(fn, "%s/.uim", pw->pw_dir);
   } else {
     fn = strdup(fn);
+  }
+
+  if (stat(fn, &st) == -1) {
+    free(fn);
+    return -1;
   }
 
   if (verbose < 1) {
