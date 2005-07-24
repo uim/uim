f29b6c01f4718b4dd3b2c475fa1ca7b55a60384c
tkng <tkng@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-07-24 20:08:11 +0000

    * sigscheme/test/test-apply.scm: Changed wrong test.

--- sigscheme/test/test-apply.scm
+++ sigscheme/test/test-apply.scm
@@ -1,8 +1,8 @@
-(load "test/unittest.scm")
+(load "./test/unittest.scm")
 
 ;; check apply
 (assert-eq? "apply check" #t (apply = '(1 1 1)))
-(assert-eq? "apply check" 6  (apply + '(1 2 (+ 1 2))))
+(assert-eq? "apply check" 6  (apply + `(1 2 ,(+ 1 2))))
 (assert-eq? "apply check" 4  (apply (lambda (x y) (+ x y)) '(1 3)))
 
 (total-report)
