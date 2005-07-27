26cd7e64e380cb8aa58690786d386d0e9d066ddf
kzk <kzk@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-07-27 06:16:56 +0000

    * sigscheme/test/test-map.scm   - using assert-equal?

--- sigscheme/test/test-map.scm
+++ sigscheme/test/test-map.scm
@@ -1,8 +1,8 @@
 (load "test/unittest.scm")
 
-(assert "basic map test1" (equal? '(2 2 2) (map cadr '((1 2) (1 2) (1 2)))))
-(assert "basic map test2" (equal? '(2 4 6) (map + '(1 2 3) '(1 2 3))))
-(assert "basic map test3" (equal? '(2 4 6) (map (lambda (x y) (+ x y))
-						'(1 2 3) '(1 2 3))))
+(assert-equal? "basic map test1" '(2 2 2) (map cadr '((1 2) (1 2) (1 2))))
+(assert-equal? "basic map test2" '(2 4 6) (map + '(1 2 3) '(1 2 3)))
+(assert-equal? "basic map test3" '(2 4 6) (map (lambda (x y) (+ x y))
+						'(1 2 3) '(1 2 3)))
 
 (total-report)
