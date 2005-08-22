2344449482c6a7568f2063ac924e9ed9097b96f1
ekato <ekato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-08-22 17:20:00 +0000

    * scm/skk.scm (skk-flush) : Flush its child context too. (skk-begin-conversion) : Bind res as #f according to [anthy-dev   2259]. (skk-proc-state-okuri) : Ditto.

--- scm/skk.scm
+++ scm/skk.scm
@@ -341,19 +341,22 @@
 
 (define skk-flush
   (lambda (sc)
-    (rk-flush (skk-context-rk-context sc))
-    (if skk-use-recursive-learning?
-	(skk-editor-flush (skk-context-editor sc)))
-    (skk-dialog-flush (skk-context-dialog sc))
-    (if (not (skk-latin-state? sc))
-	(skk-context-set-state! sc 'skk-state-direct))
-    (skk-context-set-head! sc '())
-    (skk-context-set-okuri-head! sc "")
-    (skk-context-set-okuri! sc '())
-    (skk-context-set-appendix! sc '())
-    (skk-reset-candidate-window sc)
-    (skk-context-set-nr-candidates! sc 0)
-    (skk-context-set-latin-conv! sc #f)))
+    (let ((csc (skk-context-child-context sc)))
+      (rk-flush (skk-context-rk-context sc))
+      (if skk-use-recursive-learning?
+	  (skk-editor-flush (skk-context-editor sc)))
+      (skk-dialog-flush (skk-context-dialog sc))
+      (if (not (skk-latin-state? sc))
+	  (skk-context-set-state! sc 'skk-state-direct))
+      (skk-context-set-head! sc '())
+      (skk-context-set-okuri-head! sc "")
+      (skk-context-set-okuri! sc '())
+      (skk-context-set-appendix! sc '())
+      (skk-reset-candidate-window sc)
+      (skk-context-set-nr-candidates! sc 0)
+      (skk-context-set-latin-conv! sc #f)
+      (if (not (null? csc))
+	  (skk-flush csc)))))
 
 (define skk-context-new
   (lambda (id im)
@@ -676,7 +679,7 @@
 
 (define skk-begin-completion
   (lambda (sc)
-    (let ((res))
+    (let ((res #f))
       ;; get residual 'n'
       (if (= (skk-context-state sc) 'skk-state-kanji)
 	  (skk-append-residual-kana sc))
@@ -1705,7 +1708,7 @@
   (lambda (c key key-state)
     (let* ((sc (skk-find-descendant-context c))
 	   (rkc (skk-context-rk-context sc))
-	   (res))
+	   (res #f))
       (and
        (if (skk-cancel-key? key key-state)
 	   (begin
