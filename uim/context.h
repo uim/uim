09e84a6933b7dcac04c7cbc6a251bb886f0c5114
tkng <tkng@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-08-25 22:42:11 +0000

    * uim/context.h: Added new macros for pthread mutex.  -(UIM_NEW_MUTEX, UIM_NEW_MUTEX_STATIC, UIM_LOCK_MUTEX,    UIM_UNLOCK_MUTEX): New macros.

--- uim/context.h
+++ uim/context.h
@@ -120,6 +120,20 @@ struct uim_context_ {
 };
 
 
+#ifdef HAVE_PTHREAD_H
+#include <pthread.h>
+  #define UIM_NEW_MUTEX(mtx)                pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER
+  #define UIM_NEW_MUTEX_STATIC(mtx)  static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER
+  #define UIM_LOCK_MUTEX(mtx)    pthread_mutex_lock(&mtx)
+  #define UIM_UNLOCK_MUTEX(mtx)  pthread_mutex_unlock(&mtx)
+#else
+  #define UIM_NEW_MUTEX(mtx)
+  #define UIM_NEW_MUTEX_STATIC(mtx)
+  #define UIM_LOCK_MUTEX(mtx)
+  #define UIM_UNLOCK_MUTEX(mtx)
+#endif /* HAVE_PTHREAD_H */
+
+
 #ifdef ENABLE_NLS
 #define UIM_PREPARE_SAVING_TEXTDOMAIN_CODESET() \
     char *enc, *orig_encoding = NULL; \
