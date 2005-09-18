4809220902cfef0dad5fa982436e531b24871e3a
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-09-18 08:31:12 +0000

    * This commit prepares a new efficient values passing. The   implementation will follow after the internal reorganization by Jun   Inoue
    * sigscheme/sigscheme.h
      - (SCM_USE_VALUECONS): New macro
    
    * sigscheme/sigschemetype.h
      - (ScmObjType): Add ScmValueCons
      - (struct ScmObjInternal_): Disable value_pack when
        SCM_USE_VALUECONS is true
      - (SCM_AS_VALUECONS, SCM_VALUECONSP, SCM_ENTYPE_VALUECONS,
        SCM_VALUECONS_CAR, SCM_VALUECONS_CDR): New macro

--- sigscheme/sigscheme.h
+++ sigscheme/sigscheme.h
@@ -79,6 +79,7 @@ typedef void (*ScmCFunc)(void);
 #define SCM_STRICT_ARGCHECK     0  /* enable strict argument check */
 #define SCM_ACCESSOR_ASSERT     0  /* enable strict type check with accessor */
 #define SCM_GCC4_READY_GC       0  /* use experimental gcc4-ready stack protection */
+#define SCM_USE_VALUECONS       0  /* use experimental values passing */
 
 /* dependency resolution */
 #if SCM_COMPAT_SIOD
