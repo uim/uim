f7170715f5675bdc74ce170ce7f4d6c46a88991a
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-09-24 11:27:49 +0000

    * uim/uim-scm-gc.c   - (uim_scm_gc_protect_stack): Add a comment

--- uim/uim-scm-gc.c
+++ uim/uim-scm-gc.c
@@ -45,6 +45,10 @@ uim_scm_gc_protect(uim_lisp *location)
 uim_lisp *
 uim_scm_gc_protect_stack(void)
 {
+  /*
+   * &stack_start will be relocated to start of the frame of subsequent
+   * function call
+   */
   LISP stack_start;
 
   siod_gc_protect_stack(&stack_start);
