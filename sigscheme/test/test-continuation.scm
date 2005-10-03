2c5a1d5031b2eb41c04971236d2f2995174c5a47
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-10-03 10:22:20 +0000

    * sigscheme/test/test-continuation.scm   - Insert copyright header   - Rewrite "call/cc test1" style to "call/cc #1"

--- sigscheme/test/test-continuation.scm
+++ sigscheme/test/test-continuation.scm
@@ -1,8 +1,40 @@
+;;  FileName : test-continuation.scm
+;;  About    : unit test for continuation
+;;
+;;  Copyright (C) 2005      by Kazuki Ohta (mover@hct.zaq.ne.jp)
+;;
+;;  All rights reserved.
+;;
+;;  Redistribution and use in source and binary forms, with or without
+;;  modification, are permitted provided that the following conditions
+;;  are met:
+;;
+;;  1. Redistributions of source code must retain the above copyright
+;;     notice, this list of conditions and the following disclaimer.
+;;  2. Redistributions in binary form must reproduce the above copyright
+;;     notice, this list of conditions and the following disclaimer in the
+;;     documentation and/or other materials provided with the distribution.
+;;  3. Neither the name of authors nor the names of its contributors
+;;     may be used to endorse or promote products derived from this software
+;;     without specific prior written permission.
+;;
+;;  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+;;  IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+;;  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+;;  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+;;  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;;  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+;;  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 (load "./test/unittest.scm")
 
 (define call/cc call-with-current-continuation)
 
-(assert-equal? "call/cc test1" -3  (call-with-current-continuation
+(assert-equal? "call/cc #1" -3  (call-with-current-continuation
 				 (lambda (exit)
 				   (for-each (lambda (x)
 					       (if (negative? x)
@@ -23,8 +55,8 @@
 			  (return #f))))))
       (re obj))))))
 
-(assert-equal? "call/cc test2" 4  (list-length '(1 2 3 4)))
-(assert-equal? "call/cc test3" #f (list-length '(a b . c)))
+(assert-equal? "call/cc #2" 4  (list-length '(1 2 3 4)))
+(assert-equal? "call/cc #3" #f (list-length '(a b . c)))
 
 ;; function written in C as proc
 (assert-true   "call/cc #4" (call/cc procedure?))
