853cac21c3b548d6b8c8982cdeb4f14742a3e91d
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-10-04 22:35:21 +0000

    * sigscheme/sigscheme.c   - (SigScm_Initialize_internal):     * Remove obsolete and unnecessary codes     * Add some FIXME comments

--- sigscheme/sigscheme.c
+++ sigscheme/sigscheme.c
@@ -147,26 +147,18 @@ static void SigScm_Initialize_internal(void)
     SigScm_quasiquote       = Scm_Intern("quasiquote");
     SigScm_unquote          = Scm_Intern("unquote");
     SigScm_unquote_splicing = Scm_Intern("unquote-splicing");
-#if SCM_USE_NONSTD_FEATURES
-    SigScm_features         = Scm_Intern("*features*");
-    SCM_SYMBOL_SET_VCELL(SigScm_features, SCM_NULL);
-#endif
-
 #if 0
-    /* FIXME: really required? */
-    SCM_SYMBOL_SET_VCELL(Scm_Intern("#t"),   SCM_TRUE);
-    SCM_SYMBOL_SET_VCELL(Scm_Intern("#f"),   SCM_FALSE);
-#endif
-#if 1
-    /* FIXME: don't set SCM_TRUE and rely on the value */
+    /* FIXME: Rewrite ScmExp_cond() and ScmExp_case(), and enable this */
+    SigScm_else             = Scm_Intern("else");
+    SigScm_foo              = Scm_Intern("=>");
+#else
+    /* FIXME: obsolete this. don't set SCM_TRUE and rely on the value */
     SCM_SYMBOL_SET_VCELL(Scm_Intern("else"), SCM_TRUE);
     SCM_SYMBOL_SET_VCELL(Scm_Intern("=>"),   SCM_TRUE);
-#else
-    SigScm_foo              = Scm_Intern("=>");
-    SigScm_else             = Scm_Intern("else");
 #endif
 
 #if SCM_USE_VALUECONS
+    /* FIXME: hide into datas.c */
     /*
      * To keep storage model abstract, the cell is allocated from a heap
      * instead of directly construct ScmCell
@@ -174,6 +166,11 @@ static void SigScm_Initialize_internal(void)
     SigScm_null_values = CONS(SCM_NULL, SCM_NULL);
     SCM_ENTYPE_VALUEPACKET(SigScm_null_values);
     SigScm_GC_Protect(&SigScm_null_values);
+#endif
+
+#if SCM_USE_NONSTD_FEATURES
+    SigScm_features         = Scm_Intern("*features*");
+    SCM_SYMBOL_SET_VCELL(SigScm_features, SCM_NULL);
 #endif
 
     /*=======================================================================
@@ -377,18 +374,6 @@ static void SigScm_Initialize_internal(void)
 
     Scm_RegisterSyntaxFixed1("use"                         , ScmExp_use);
     Scm_DefineAlias("call/cc", "call-with-current-continuation");
-#endif
-
-#if 0
-    /* enable all features as backward compatibility */
-    ScmExp_use(Scm_Intern("srfi-1"), SCM_INTERACTION_ENV);
-    ScmExp_use(Scm_Intern("srfi-2"), SCM_INTERACTION_ENV);
-    ScmExp_use(Scm_Intern("srfi-8"), SCM_INTERACTION_ENV);
-    ScmExp_use(Scm_Intern("srfi-23"), SCM_INTERACTION_ENV);
-    ScmExp_use(Scm_Intern("srfi-34"), SCM_INTERACTION_ENV);
-    ScmExp_use(Scm_Intern("srfi-38"), SCM_INTERACTION_ENV);
-    ScmExp_use(Scm_Intern("srfi-60"), SCM_INTERACTION_ENV);
-    ScmExp_use(Scm_Intern("siod"), SCM_INTERACTION_ENV);
 #endif
 
 #if SCM_EXCEPTION_HANDLING
