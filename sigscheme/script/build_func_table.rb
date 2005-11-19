f3654556242e119b8594037bc2b0e37364effd3c
kzk <kzk@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-11-19 05:09:36 +0000

    * sigscheme/Makefile.am   - add scm_decl.rb to BUILD_FUNCTBL_SOURCES
    * sigscheme/script/build_func_table.rb
      - remove duplicate code and require scm_decl.rb

--- sigscheme/script/build_func_table.rb
+++ sigscheme/script/build_func_table.rb
@@ -32,72 +32,7 @@
 #  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 #  SUCH DAMAGE.
 #===========================================================================
-
-# $1  :prototype       ScmObj ScmOp_call_with_values(ScmObj producer, ScmObj consumer, ScmEvalState *eval_state)
-# $2  :ret             ScmObj
-# $3  :func            ScmOp_call_with_values
-# $4  :prefix          Op
-# $5  :func_body       call_with_values
-# $6  :args            ScmObj producer, ScmObj consumer, ScmEvalState *eval_state
-# $7  :proc            call-with-values
-# $8  :register_func   ProcedureFixedTailRec2
-# $9  :functype_prefix Procedure
-# $10 :functype_spec   FixedTailRec2
-SCM_DECL_RE = /\n((ScmObj)\s+(Scm(Op|Exp)_(\w+))\(([^{]+)\))[ \t]*\n\s*\{[^{}]+DECLARE_FUNCTION\(\s*\"([^\"]+)\"[\s,]+([^\s,]+)\)/m
-
-class String
-  def scan_scm_decl
-    res = []
-    scan(SCM_DECL_RE) { |prototype, ret, func, prefix, func_body, args, proc, register_func, functype_prefix, functype_spec|
-      decl = {
-        :prototype       => prototype.gsub(/\s+/, " "),
-        :ret             => ret,
-        :func            => func,
-        :prefix          => prefix,
-        :func_body       => func_body,
-        :args            => args.gsub(/\s+/, " "),
-        :proc            => proc,
-        :register_func   => "Scm_Register" + register_func,
-        :functype_prefix => functype_prefix,
-        :functype_spec   => functype_spec,
-      }
-      res << yield(decl)
-    }
-    res
-  end
-end
-
-def scm_func_table_entry(decl)
-  proc, func, register_func = decl.values_at(:proc, :func, :register_func)
-  "{ \"#{proc}\", (ScmBuiltinFunc)#{func}, (ScmRegisterFunc)#{register_func} }"
-end
-
-def scm_func_register_exp(decl)
-  proc, func, register_func = decl.values_at(:proc, :func, :register_func)
-  "#{register_func}(\"#{proc}\", #{func})"
-end
-
-def scm_generate_func_table_body(str)
-  str.scan_scm_decl { |decl|
-    entry = scm_func_table_entry(decl)
-    "    #{entry},\n"
-  }.join
-end
-
-def scm_generate_func_register_exps(str)
-  str.scan_scm_decl { |decl|
-    exp = scm_func_register_exp(decl)
-    "    #{exp};\n"
-  }.join
-end
-
-def scm_generate_func_prototypes(str)
-  str.scan_scm_decl { |decl|
-    "#{decl[:prototype]};\n"
-  }.join
-end
-
-#####################################################################
+require 'script/scm_decl.rb'
 
 def search_declare_function(filename)
   puts "    /* #{filename} */"
