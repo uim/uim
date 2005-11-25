0a18b51119e7996a554135ae8add87b2dc653b68
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-11-25 06:12:52 +0000

    * sigscheme/test/test-enc-utf8.scm * sigscheme/test/test-enc-eucgeneric.scm * sigscheme/test/test-enc-eucjp.scm * sigscheme/test/test-enc-sjis.scm   - Add encoding specification using the UNIX script prelude   - Make executable   - Insert license header

--- sigscheme/test/test-enc-eucgeneric.scm
+++ sigscheme/test/test-enc-eucgeneric.scm
@@ -1,3 +1,38 @@
+#! /usr/bin/env sscm -C EUC-CN
+;; -*- euc-cn -*-
+
+;;  FileName : test-enc-eucgeneric.scm
+;;  About    : unit test for EUC string
+;;
+;;  Copyright (C) 2005      by Kazuki Ohta (mover@hct.zaq.ne.jp)
+;;
+;;  All rights reserved.
+;;
+;;  Redistribution and use in source and binary forms, with or without
+;;  modification, are permitted provided that the following conditions
+;;  are met:
+;;
+;;  1. Redistributions of source code must retain the above copyright
+;;     notice, this list of conditions and the following disclaimer.
+;;  2. Redistributions in binary form must reproduce the above copyright
+;;     notice, this list of conditions and the following disclaimer in the
+;;     documentation and/or other materials provided with the distribution.
+;;  3. Neither the name of authors nor the names of its contributors
+;;     may be used to endorse or promote products derived from this software
+;;     without specific prior written permission.
+;;
+;;  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+;;  IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+;;  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+;;  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+;;  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;;  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+;;  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 (load "./test/unittest.scm")
 
 ;; This file provides a fallback test unit for all EUC systems.  It's
@@ -21,3 +56,5 @@
 
 (assert-equal? "string 2" str1 (apply string str1-list))
 (assert-equal? "list->string 2" str1-list (string->list str1))
+
+(total-report)
