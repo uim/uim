4fb4d97a8a3db7ac456f73016f3eb30f0494a3a7
ekato <ekato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-12-04 07:11:16 +0000

    * xim/compose.cpp (XimIM::get_compose_filename) : Use fallback   directory for compose files on x86_64 Linux.

--- xim/compose.cpp
+++ xim/compose.cpp
@@ -52,6 +52,7 @@
 #define COMPOSE_FILE	"Compose"
 #define COMPOSE_DIR_FILE	"X11/locale/compose.dir"
 #define XLOCALE_DIR	"X11/locale"
+#define FALLBACK_XLIB_DIR	"/usr/X11R6/lib"
 
 #define XLC_BUFSIZE	256
 #define iscomment(ch)	((ch) == '#' || (ch) == '\0')
@@ -734,6 +735,7 @@ char *XimIM::get_compose_filename()
 
     FILE *fp;
     char buf[XLC_BUFSIZE], *name = NULL, *filename = NULL;
+    const char *xlib_dir = XLIB_DIR ;
 
     lang_region = get_lang_region();
     encoding = get_encoding();
@@ -755,9 +757,29 @@ char *XimIM::get_compose_filename()
 
     fp = fopen(compose_dir_file, "r");
     if (fp == NULL) {
-	free(locale);
-	free(compose_dir_file);
-	return NULL;
+	/* retry with fallback file */
+	if (strcmp(FALLBACK_XLIB_DIR, XLIB_DIR)) {
+	    compose_dir_file = (char *)realloc(compose_dir_file,
+			    strlen(FALLBACK_XLIB_DIR) +
+			    strlen(COMPOSE_DIR_FILE) + 2);
+	    if (compose_dir_file == NULL) {
+		free(locale);
+		return NULL;
+	    }
+	    sprintf(compose_dir_file, "%s/%s",
+			    FALLBACK_XLIB_DIR, COMPOSE_DIR_FILE);
+	    fp = fopen(compose_dir_file, "r");
+	    if (fp == NULL) {
+		free(locale);
+		free(compose_dir_file);
+		return NULL;
+	    }
+	    xlib_dir = FALLBACK_XLIB_DIR;
+	} else {
+	    free(locale);
+	    free(compose_dir_file);
+	    return NULL;
+	}
     }
 
     while (fgets(buf, XLC_BUFSIZE, fp) != NULL) {
@@ -790,10 +812,11 @@ char *XimIM::get_compose_filename()
     if (name == NULL)
 	return NULL;
 
-    filename = (char *)malloc(sizeof(char) * (strlen(XLIB_DIR) + strlen(XLOCALE_DIR) + strlen(name) + 3));
+    filename = (char *)malloc(strlen(xlib_dir) + strlen(XLOCALE_DIR) +
+		    strlen(name) + 3);
     if (filename == NULL)
 	return NULL;
-    sprintf(filename, "%s/%s/%s", XLIB_DIR, XLOCALE_DIR, name);
+    sprintf(filename, "%s/%s/%s", xlib_dir, XLOCALE_DIR, name);
     free(name);
 
     return filename;
