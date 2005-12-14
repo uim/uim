8e81d5d827084b1c2bfd923c0cfe19cb70740205
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2005-12-14 18:59:51 +0000

    * sigscheme/error.c   - (ERRMSG_UNHANDLED_EXCEPTION, ERRMSG_MEMORY_EXHAUSTED): Rename to     SCM_ERRMSG_*   - (SCM_ERRMSG_UNHANDLED_EXCEPTION, SCM_ERRMSG_MEMORY_EXHAUSTED):     Renamed from ERRMSG_*   - (SCM_ASSERT_ALLOCATED):     * Renamed from ASSERT_ALLOCATED     * Replace ERR with Scm_FatalError()   - (ASSERT_ALLOCATED): Redefine as alias to SCM_ASSERT_ALLOCATED   - (scm_malloc, scm_calloc, scm_realloc): Replace manual error     handling with ASSERT_ALLOCATED()   - (ScmOp_inspect_error): Follow the renaming of     ERRMSG_UNHANDLED_EXCEPTION

--- sigscheme/error.c
+++ sigscheme/error.c
@@ -55,10 +55,12 @@
 #define SCM_BACKTRACE_HEADER "**** BACKTRACE ****\n"
 #define SCM_BACKTRACE_SEP    "------------------------------\n"
 
-#define ERRMSG_UNHANDLED_EXCEPTION "unhandled exception"
-#define ERRMSG_MEMORY_EXHAUSTED    "memory exhausted"
+#define SCM_ERRMSG_UNHANDLED_EXCEPTION "unhandled exception"
+#define SCM_ERRMSG_MEMORY_EXHAUSTED    "memory exhausted"
 
-#define ASSERT_ALLOCATED(p) ((p) || (ERR(ERRMSG_MEMORY_EXHAUSTED), 1))
+#define SCM_ASSERT_ALLOCATED(p)                                              \
+    ((p) || (Scm_FatalError(SCM_ERRMSG_MEMORY_EXHAUSTED), 1))
+#define ASSERT_ALLOCATED SCM_ASSERT_ALLOCATED
 
 /*=======================================
   Variable Declarations
@@ -154,8 +156,7 @@ void *scm_malloc(size_t size)
     void *p;
 
     p = malloc(size);
-    if (!p)
-        Scm_FatalError(ERRMSG_MEMORY_EXHAUSTED);
+    ASSERT_ALLOCATED(p);
 
     return p;
 }
@@ -165,8 +166,7 @@ void *scm_calloc(size_t number, size_t size)
     void *p;
 
     p = calloc(number, size);
-    if (!p)
-        Scm_FatalError(ERRMSG_MEMORY_EXHAUSTED);
+    ASSERT_ALLOCATED(p);
 
     return p;
 }
@@ -176,8 +176,7 @@ void *scm_realloc(void *ptr, size_t size)
     void *p;
 
     p = realloc(ptr, size);
-    if (!p)
-        Scm_FatalError(ERRMSG_MEMORY_EXHAUSTED);
+    ASSERT_ALLOCATED(p);
 
     return p;
 }
@@ -289,7 +288,8 @@ ScmObj ScmOp_inspect_error(ScmObj err_obj)
         if (ERROBJP(err_obj)) {
             SigScm_DisplayToPort(scm_current_error_port, err_obj);
         } else {
-            SCM_PORT_PUTS(scm_current_error_port, ERRMSG_UNHANDLED_EXCEPTION);
+            SCM_PORT_PUTS(scm_current_error_port,
+                          SCM_ERRMSG_UNHANDLED_EXCEPTION);
             SCM_PORT_PUTS(scm_current_error_port, ": ");
             SigScm_WriteToPort(scm_current_error_port, err_obj);
         }
