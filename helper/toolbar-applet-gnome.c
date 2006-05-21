cf1533e85ced52243f926b54404560aef354987a
ekato <ekato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-05-21 14:16:51 +0000

    * helper/toolbar-applet-gnome.c : Revise the fix for bug #6963.   - (applet_background_changed_cb) : Removed.   - (uim_applet_new) : Use libpanel-applet's functionality.

--- helper/toolbar-applet-gnome.c
+++ helper/toolbar-applet-gnome.c
@@ -137,41 +137,6 @@ display_about_dialog(BonoboUIComponent *uic, gpointer data,
   gtk_widget_show(dialog);
 }
 
-static void applet_background_changed_cb(PanelApplet *applet,
-					 PanelAppletBackgroundType type,
-					 GdkColor *color,
-					 GdkPixmap *pixmap,
-					 gpointer data)
-{
-  GtkRcStyle *rc_style;
-  GtkStyle *style;
-
-  /* reset style */
-  gtk_widget_set_style(GTK_WIDGET(applet), NULL);
-  rc_style = gtk_rc_style_new();
-  gtk_widget_modify_style(GTK_WIDGET(applet), rc_style);
-  gtk_rc_style_unref (rc_style);
-
-  switch (type) {
-  case PANEL_PIXMAP_BACKGROUND:
-    style = gtk_style_copy(GTK_WIDGET(applet)->style);
-    if (style->bg_pixmap[GTK_STATE_NORMAL])
-      g_object_unref(style->bg_pixmap[GTK_STATE_NORMAL]);
-    style->bg_pixmap[GTK_STATE_NORMAL] = g_object_ref(pixmap);
-    gtk_widget_set_style(GTK_WIDGET(applet), style);
-    g_object_unref(style);
-    break;
-
-  case PANEL_COLOR_BACKGROUND:
-    gtk_widget_modify_bg(GTK_WIDGET(applet), GTK_STATE_NORMAL, color);
-    break;
-
-  case PANEL_NO_BACKGROUND:
-  default:
-    break;
-  }
-}
-
 static gboolean
 uim_applet_new(PanelApplet *applet, const gchar *iid, gpointer data)
 {
@@ -186,12 +151,11 @@ uim_applet_new(PanelApplet *applet, const gchar *iid, gpointer data)
   toolbar = (GtkWidget*)uim_toolbar_applet_new();
 
   gtk_container_add(GTK_CONTAINER(applet), toolbar);
-  g_signal_connect(G_OBJECT(applet), "change_background",
-		   G_CALLBACK(applet_background_changed_cb), NULL);
 
   gtk_widget_show_all(GTK_WIDGET(applet));
 
   panel_applet_setup_menu(applet, uim_menu_xml, uim_menu_verbs, toolbar);
+  panel_applet_set_background_widget(applet, GTK_WIDGET(applet));
 
   return TRUE;
 }
