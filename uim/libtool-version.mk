e296c38484c539e519080de7d3721d474f01fb92
ekato <ekato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-08-01 03:28:28 +0000

    * qt/Makefile.am * replace/os_dep.h * uim/libtool-version.mk * uim/Makefile.am   - Port r3798:3800 from trunk.

--- uim/libtool-version.mk
+++ uim/libtool-version.mk
@@ -1,5 +1,5 @@
 # The versions consists of CURRENT:REVISION:AGE to indicate ABI information.
 # See "Versioning" section of info of libtool for each meaning.
 
-libuim_version = 2:1:1
+libuim_version = 3:0:0
 libuim_custom_version = 2:0:0
