1ba1f00565f81bb4bbff68c9db972b8d0ac33609
ekato <ekato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-08-04 04:13:28 +0000

    * configure.ac * uim/uim.h   - Post release bump version.

--- uim/uim.h
+++ uim/uim.h
@@ -42,7 +42,7 @@ extern "C" {
 #endif
 
 #define UIM_VERSION_MAJOR      (1)
-#define UIM_VERSION_MINOR      (2)
+#define UIM_VERSION_MINOR      (3)
 #define UIM_VERSION_PATCHLEVEL (0)
 #define UIM_VERSION_REQUIRE(major, minor, patchlevel)			     \
   ((major) < UIM_VERSION_MAJOR						     \
