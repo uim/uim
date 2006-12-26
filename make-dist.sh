cd8361da49d9cce4e2e64b5c77b7e1d8015ceb66
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-12-26 22:27:36 +0000

    * configure.ac   - Inhibit $ac_configure_args overriding for sigshceme again * make-dist.sh   - Remove --enable-conf=full from configure args

--- make-dist.sh
+++ make-dist.sh
@@ -4,15 +4,17 @@ MAKE=make
 
 UIM_REPOSITORY="http://anonsvn.freedesktop.org/svn/uim"
 SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
+LIBGCROOTS_REPOSITORY="${UIM_REPOSITORY}/libgcroots-trunk"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
-#SSCM_URL="${TAGS_REPOSITORY}/sigscheme-0.7.2"
-SSCM_URL="${UIM_REPOSITORY}/sigscheme-trunk"
+SSCM_URL="${TAGS_REPOSITORY}/sigscheme-0.7.2"
+#SSCM_URL="$SSCM_REPOSITORY}"
 LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.2"
+#LIBGCROOTS_URL="${LIBGCROOTS_REPOSITORY}"
 
 svn export $SSCM_URL sigscheme
 svn export $LIBGCROOTS_URL sigscheme/libgcroots
 (cd sigscheme/libgcroots && ./autogen.sh) \
  && (cd sigscheme && ./autogen.sh) \
  && ./autogen.sh
-./configure --enable-maintainer-mode --enable-conf=full
+./configure --enable-maintainer-mode
 $MAKE distcheck sum
