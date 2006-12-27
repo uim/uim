7ba10d84426be668d0bbb65163eee6e87325d77e
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-12-27 09:29:02 +0000

    * make-dist.sh   - Add 'make distcheck' with various argument sets * Makefile.am   - (releasetest-all-flag-enabled, releasetest-all-flag-disabled,     each-releasetest, releasetest): Remove the distcheck-alternative     workaround. Use make-dist.sh instead   - Cosmetic changes * doc/RELEASING   - Update

--- make-dist.sh
+++ make-dist.sh
@@ -11,10 +11,20 @@ SSCM_URL="${TAGS_REPOSITORY}/sigscheme-0.7.2"
 LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.2"
 #LIBGCROOTS_URL="${LIBGCROOTS_REPOSITORY}"
 
+CONF_COMMON="--enable-maintainer-mode"
+CONF_NONE="$CONF_COMMON --disable-debug --disable-fep --disable-emacs --disable-dict --disable-scim --disable-gnome-applet --without-anthy --without-canna --without-mana --without-prime --without-m17nlib --without-gtk2 --without-gnome2 --without-qt --without-qt-immodule --disable-compat-scm --without-eb --without-libedit"
+CONF_DEFAULT="$CONF_COMMON"
+CONF_FULL="$CONF_COMMON --enable-debug --enable-fep --enable-emacs --enable-dict --enable-scim --enable-gnome-applet --with-anthy --with-canna --with-mana --with-prime --with-m17nlib --with-gtk2 --with-gnome2 --with-qt --with-qt-immodule --enable-compat-scm --with-eb --with-libedit"
+
+
 svn export $SSCM_URL sigscheme
 svn export $LIBGCROOTS_URL sigscheme/libgcroots
 (cd sigscheme/libgcroots && ./autogen.sh) \
  && (cd sigscheme && ./autogen.sh) \
  && ./autogen.sh
-./configure --enable-maintainer-mode
-$MAKE distcheck sum
+
+for conf_args in "$CONF_NONE" "$CONF_DEFAULT" "$CONF_FULL"; do
+    echo "configure $conf_args"
+    ./configure $conf_args && $MAKE distcheck sum \
+      || { echo 'make distcheck failed'; exit 1; }
+done
