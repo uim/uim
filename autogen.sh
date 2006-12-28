51b29549196d5f142efc7520f093c0e484d6bd50
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-12-28 04:15:56 +0000

    * qt/chardict/po/Makevars   - (GETTEXT_PACKAGE): New variable to adapt to recent intltool * configure.ac   - Require intltool 0.35.2 * autogen.sh   - Apply "# Makevars" workaround to po/Makefile.in.in   - Copy po/Makefile.in.in to qt/chardict/po * qt/chardict/po/Makefile.in.in   - Removed

--- autogen.sh
+++ autogen.sh
@@ -8,4 +8,10 @@ aclocal -I m4 \
   && automake --add-missing --foreign --copy \
   && autoconf \
   && intltoolize --copy --force --automake \
-  && perl -i -pe 's/\bscm\b/scm-workaround/g' intltool-update.in
+  && perl -i -pe 's/\bscm\b/scm-workaround/g' intltool-update.in \
+  && perl -i -pe "s/^INSTALL\b/# Makevars gets inserted here. (Don't remove this line!)\n\nINSTALL/" po/Makefile.in.in \
+  && cp po/Makefile.in.in qt/chardict/po
+
+# Since intltool 0.32 and later does not include po/Makevars into Makefile by
+# default as we expected, I do the '# Makevars' workaround here. See
+# config.status for further details.  -- YamaKen 2006-12-28
