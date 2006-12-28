58819ac45198b58c9af006c08cfb3110d1d16e12
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2006-12-28 20:46:00 +0000

    * This commit make 'make distcheck' passed
    * configure.ac
      - Add IT_PO_SUBDIR([qt/chardict/po])
    * autogen.sh
      - Add patch for -m with (srcdir != builddir) for intltool-update.in
    * qt/chardict/po/Makevars
      - (top_builddir): Removed and replaced with Makefile.in.in -provided one
      - (top_podir): New variable
      - Override MSGMERGE and GENPOT
    * po/POTFILES.in
      - Add helper/eggtrayicon.c
    * po/POTFILES.skip
      - New file

--- autogen.sh
+++ autogen.sh
@@ -8,10 +8,31 @@ aclocal -I m4 \
   && automake --add-missing --foreign --copy \
   && autoconf \
   && intltoolize --copy --force --automake \
+  && perl -i -pe "s/^DISTFILES\b/# Makevars gets inserted here. (Don't remove this line!)\n\nDISTFILES/" po/Makefile.in.in \
   && perl -i -pe 's/\bscm\b/scm-workaround/g' intltool-update.in \
-  && perl -i -pe "s/^INSTALL\b/# Makevars gets inserted here. (Don't remove this line!)\n\nINSTALL/" po/Makefile.in.in \
-  && cp po/Makefile.in.in qt/chardict/po
+  && perl -i -pe 's%"(POTFILES.(skip|ignore))"%"\$SRCDIR/$1"%g' intltool-update.in \
+  && perl -i -pe 's%(-f "\$SRCDIR/../\$dummy")%$1 and ! -f "../\$dummy"%' intltool-update.in \
+  && cp po/Makefile.in.in qt/chardict/po \
+  && ed qt/chardict/po/Makefile.in.in <<EOT
+/^check:
++
+.,/^$/d
+i
+# The test is removed for qt/chardict/po  -- YamaKen
+
+.
+wq
+EOT
 
 # Since intltool 0.32 and later does not include po/Makevars into Makefile by
 # default as we expected, I do the '# Makevars' workaround here. See
 # config.status for further details.  -- YamaKen 2006-12-28
+
+# To cancel the unwanted special treatment for *.scm by intltool-update, change
+# the suffix here.  -- YamaKen 2006-12-29
+
+# To fix the case intltool-update -m on (srcdir != builddir), the
+# $SRCDIR-related lines are patched here.  -- YamaKen 2006-12-29
+
+# qt/chardict/po has separated gettext package with a dirty workaround.
+#   -- YamaKen 2006-12-29
