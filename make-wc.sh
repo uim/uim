7006836045f69e46cc5b26f7f313744d65a42cf2
yamaken <yamaken@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2007-01-09 07:18:29 +0000

    * make-wc.sh   - New file copied from make-dist.sh * configure.ac   - Increase version to 1.5.0 * uim/libtool-version.mk   - (libuim_version): Increase to 6:0:0 from 5:0:0 due to the uim-scm     incompatibility
    * doc/COMPATIBILITY
      - Fix previous commit

---
+++ make-wc.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+# make-wc.sh: Check out working copy of uim trunk and prepare to build.  Do
+#             not use this script to make a release. Use make-dist.sh for it.
+#
+# Usage:
+#
+#     $ svn co http://anonsvn.freedesktop.org/svn/uim/trunk uim-trunk
+#     $ cd uim-trunk
+#     $ ./make-wc.sh
+#     $ make
+#
+#   Or if you are an uim committer,
+#
+#     $ svn co svn+ssh://svn.freedesktop.org/svn/uim uim-trunk
+#     $ cd uim-trunk
+#     $ ./make-wc.sh
+#     $ make
+#
+#   You can add arbitrary configure options to make-wc.sh as follows:
+#
+#     $ ./make-wc.sh --enable-debug --enable-backtrace
+
+MAKE=make
+
+#UIM_REPOSITORY="http://anonsvn.freedesktop.org/svn/uim"
+#UIM_REPOSITORY="svn+ssh://svn.freedesktop.org/svn/uim"
+# autoselect
+UIM_REPOSITORY=`svn info | grep 'Repository Root:' | sed 's/.*: //'`
+
+SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
+LIBGCROOTS_REPOSITORY="${UIM_REPOSITORY}/libgcroots-trunk"
+TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
+SSCM_URL="${SSCM_REPOSITORY}"
+#SSCM_URL="${TAGS_REPOSITORY}/sigscheme-0.7.3"
+LIBGCROOTS_URL="${LIBGCROOTS_REPOSITORY}"
+#LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.1.4"
+RELEASE_SUFFIX="-snapshot-"`date +%Y%m%d`
+
+# --enable-maintainer-mode is required to build svn trunk
+CONF_COMMON="--enable-maintainer-mode --disable-warnings-into-error"
+
+
+svn co $SSCM_URL sigscheme
+svn co $LIBGCROOTS_URL sigscheme/libgcroots
+(cd sigscheme/libgcroots && ./autogen.sh) \
+ && (cd sigscheme && ./autogen.sh) \
+ && ./autogen.sh \
+|| { echo 'autogen failed.' && exit 1; }
+
+if test -n "$RELEASE_SUFFIX"; then
+    ed Makefile.in <<EOT
+/^distdir =
+d
+i
+RELEASE_SUFFIX = ${RELEASE_SUFFIX}
+#distdir = \$(PACKAGE)-\$(VERSION)
+distdir = \$(PACKAGE)-\$(VERSION)\$(RELEASE_SUFFIX)
+.
+wq
+EOT
+fi
+
+echo "configure $CONF_COMMON $@"
+./configure $CONF_ARGS $@
