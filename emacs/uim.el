7cc5a6705ebe3f6507be2de1ff2816bad5184458
sasugaanija <sasugaanija@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2007-12-14 14:47:27 +0000

    * emacs/uim.el   - (uim-update-font):     * Now, this function can be called as a command.     * Do nothing if the Emacs is running on the terminal.

--- emacs/uim.el
+++ emacs/uim.el
@@ -823,21 +823,23 @@
 ;; Update font 
 ;;
 (defun uim-update-font ()
-  (mapcar 
-   '(lambda (x)
-      (let ((font (or (cdr (assq 'font (frame-parameters)))
-		      (face-font 'default))))
-	(set-face-font x font)))
+  (interactive)
+  (if window-system
+      (mapcar 
+       '(lambda (x)
+	  (let ((font (or (cdr (assq 'font (frame-parameters)))
+			  (face-font 'default))))
+	    (set-face-font x font)))
 		     
-   '(uim-preedit-face 
-     uim-preedit-underline-face 
-     uim-preedit-highlight-face
-     uim-preedit-highlight-underline-face
-     uim-separator-face
-     uim-candidate-odd-face
-     uim-candidate-even-face
-     uim-candidate-selected-face
-     uim-candidate-nth-face)))
+       '(uim-preedit-face 
+	 uim-preedit-underline-face 
+	 uim-preedit-highlight-face
+	 uim-preedit-highlight-underline-face
+	 uim-separator-face
+	 uim-candidate-odd-face
+	 uim-candidate-even-face
+	 uim-candidate-selected-face
+	 uim-candidate-nth-face))))
 
 
 (defun uim-freeze-buffer ()
