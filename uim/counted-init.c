ed6e6ae61d61962e1ce3e20f573660345d713b87
ek.kato <ek.kato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2008-05-12 15:24:03 +0000

    * uim/counted-init.h * uim/counted-init.c   - (uim_counted_init)   - (uim_counted_quit)   - (uim_force_quit_regardless_of_count)     - New wrapper functions for uim_init() and uim_quit() (bug       #15853). * uim/Makefile.am   - (noinst_LTLIBRARIES) : Add libuim-counted-init.la.   - (libuim_counted_init_la_SOURCES) : New.   - (libuim_counted_init_la_CPPFLAGS) : New. * qt/immodule-plugin.cpp   - (UimInputContextPlugin::uimInit)   - (UimInputContextPlugin::uimQuit)     - Use wrapper function of uim_init() and uim_quit() (bug       #15853). * qt/pref-qt.cpp   - (UimPrefDialog::UimPrefDialog)   - (UimPrefDialog::~UimPrefDialog)     - Likewise. * qt/immodule-quiminputcontext.h * qt/immodule-quiminputcontext.cpp   - (QUimInputContext::reloadUim) : Remove obsolete function. * qt/Makefile.am   - (libquiminputcontextplugin_la_LIBADD)   - (uim_pref_qt_LDADD)     - Use libuim-counted-init.la.

---
+++ uim/counted-init.c
@@ -0,0 +1,62 @@
+/*
+
+Copyright (c) 2008 uim Project http://code.google.com/p/uim/
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+3. Neither the name of authors nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+*/
+#include <config.h>
+
+#include "uim.h"
+#include "counted-init.h"
+
+unsigned int uim_init_count;
+
+int uim_counted_init()
+{
+  uim_init_count++;
+  if (uim_init_count != 1)
+    return 0;
+  
+  return uim_init();
+}
+
+void uim_counted_quit()
+{
+  uim_init_count--;
+  if (uim_init_count != 0)
+    return;
+  
+  uim_quit();
+}
+
+void uim_force_quit_regardless_of_count()
+{
+  uim_init_count = 0;
+  uim_quit();
+}
