bf683947fa845422ac2d96af1ab5399b006d8680
iratqq <iratqq@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2008-12-07 18:01:12 +0000

    * uim/libtool-version.mk (libuim_version)   Minor crank.

--- uim/libtool-version.mk
+++ uim/libtool-version.mk
@@ -1,6 +1,6 @@
 # The versions consists of CURRENT:REVISION:AGE to indicate ABI information.
 # See "Versioning" section of info of libtool for each meaning.
 
-libuim_version = 6:0:0
+libuim_version = 6:1:0
 libuim_scm_version = 0:0:0
 libuim_custom_version = 2:0:0
