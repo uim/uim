1a45c6ede7db3adabb02868520a5da1abdb2af15
koutou <koutou@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2008-12-12 13:30:48 +0000

    * test/util/test-uim.scm: For new test suite.

---
+++ test/util/test-uim.scm
@@ -0,0 +1,106 @@
+;;; Copyright (c) 2003-2008 uim Project http://code.google.com/p/uim/
+;;;
+;;; All rights reserved.
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions
+;;; are met:
+;;; 1. Redistributions of source code must retain the above copyright
+;;;    notice, this list of conditions and the following disclaimer.
+;;; 2. Redistributions in binary form must reproduce the above copyright
+;;;    notice, this list of conditions and the following disclaimer in the
+;;;    documentation and/or other materials provided with the distribution.
+;;; 3. Neither the name of authors nor the names of its contributors
+;;;    may be used to endorse or promote products derived from this software
+;;;    without specific prior written permission.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+;;; ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+;;; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+;;; OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;;; HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+;;; LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+;;; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+;;; SUCH DAMAGE.
+;;;
+
+(define-module test.util.test-r5rs
+  (use test.unit.test-case)
+  (use test.uim-test-utils-new))
+(select-module test.util.test-r5rs)
+
+(define (setup)
+  (uim-test-setup))
+
+(define (teardown)
+  (uim-test-teardown))
+
+(define (test-make-scm-pathname)
+  (assert-equal (uim '(string-append (load-path) "/"))
+                (uim '(make-scm-pathname "")))
+  (assert-equal (uim '(string-append (load-path) "/file"))
+                (uim '(make-scm-pathname "file")))
+  (assert-equal "/absolute/path/file"
+                (uim '(make-scm-pathname "/absolute/path/file")))
+  (assert-equal "/"
+                (uim '(make-scm-pathname "/")))
+  #f)
+
+(define (test-interaction-environment)
+  (assert-true  (uim-bool '(eval '(symbol-bound? 'filter-map)
+                                 (interaction-environment))))
+  (assert-false (uim-bool '(eval '(symbol-bound? 'filter-baz)
+                                 (interaction-environment))))
+  ;; SigScheme: syntactic keyword 'define' cannot be evaluated as value
+  (uim '(eval (list 'define 'filter-baz filter-map)
+              (interaction-environment)))
+  (assert-true  (uim-bool '(eval '(symbol-bound? 'filter-baz)
+                                 (interaction-environment))))
+  (assert-true  (uim-bool '(eq? filter-baz filter-map)))
+  #f)
+
+(define (test-%%enclose-another-env)
+  (assert-equal 3
+                (uim '(let* ((x 1)
+                             (y 2)
+                             (closure (lambda ()
+                                        (+ x y))))
+                        (closure))))
+  (assert-equal 10
+                (uim '(let* ((x 1)
+                             (y 2)
+                             (closure (lambda ()
+                                        (+ x y)))
+                             ;; SIOD: broken frame for SigScheme
+                             ;;			      (another-env '((x . 4)
+                             ;;					     (y . 6)))
+                             ;; SigScheme: valid 2-frame env
+                             (another-env '(((x) . (4))
+                                            ((y) . (6))))
+                             )
+                        (set! closure
+                              (%%enclose-another-env closure another-env))
+                        (closure))))
+  ;; causes error since z is not exist in the another-env
+  (assert-error (lambda ()
+                  (uim '(let* ((x 1)
+                               (y 2)
+                               (z 3)
+                               (closure (lambda ()
+                                          (+ x y z)))
+                               ;; SIOD: broken frame for SigScheme
+                               ;;				(another-env '((x . 4)
+                               ;;					       (y . 6)))
+                               ;; SigScheme: valid 2-frame env
+                               (another-env '(((x) . (4))
+                                              ((y) . (6))))
+                               )
+                          (set! closure
+                                (%%enclose-another-env closure another-env))
+                          (closure)))))
+  #f)
+
+(provide "test/util/test-uim")
