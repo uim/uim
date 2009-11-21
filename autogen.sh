e1496f781530bf76785ceaf3dd549b5f0f28a76a
ek.kato <ek.kato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2009-11-21 13:11:23 +0000

    * autogen.sh : Merge -r6007:6033 from trunk.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,49 @@
 #!/bin/sh
 
+generate_makefile_in_in() {
+  cp po/Makefile.in.in $1 \
+    && perl -i -pe 's/check: all.*$/check: all/' $1/Makefile.in.in \
+    && ed $1/Makefile.in.in <<EOT
+/^check:
++
+.,/^$/d
+i
+# The test is removed for $1  -- YamaKen
+
+.
+wq
+EOT
+}
+
+fix_aclocal_m4() {
+  # need workaround if intltool >= 0.40.4
+  if test $INTLTOOL_VERSION_MAJOR -eq 0 \
+       -a $INTLTOOL_VERSION_MINOR -eq 40 \
+       -a $INTLTOOL_VERSION_MICRO -gt 3 \
+     -o \
+          $INTLTOOL_VERSION_MAJOR -eq 0 \
+       -a $INTLTOOL_VERSION_MINOR -gt 40; then
+    echo "modify aclocal.m4"
+    ed aclocal.m4 << EOT
+/^    \[sed '\/\^POTFILES
+i
+    mv "\$1/Makefile" "\$1/Makefile.tmp"
+.
+/\$1\/Makefile.in
+s/Makefile.in/Makefile.tmp/
+wq
+EOT
+  fi
+}
+
+# check intltool version to examine whether fixes for aclocal.m4 is needed
+INTLTOOL_VERSION=`intltoolize --version | sed -e "s/.* \([0-9]*\.[0-9]*\.[0-9]*\)/\1/"`
+INTLTOOL_VERSION_MAJOR=`echo $INTLTOOL_VERSION | cut -d '.' -f 1`
+INTLTOOL_VERSION_MINOR=`echo $INTLTOOL_VERSION | cut -d '.' -f 2`
+INTLTOOL_VERSION_MICRO=`echo $INTLTOOL_VERSION | cut -d '.' -f 3`
+
 aclocal -I m4 \
+  && fix_aclocal_m4 \
   && libtoolize --force --copy \
   && autoheader \
   && automake --add-missing --foreign --copy \
@@ -10,18 +53,8 @@ aclocal -I m4 \
   && perl -i -pe 's/\bscm\b/scm-workaround/g' intltool-update.in \
   && perl -i -pe 's%"(POTFILES.(skip|ignore))"%"\$SRCDIR/$1"%g' intltool-update.in \
   && perl -i -pe 's%(-f "\$SRCDIR/../\$dummy")%$1 and ! -f "../\$dummy"%' intltool-update.in \
-  && cp po/Makefile.in.in qt/chardict/po \
-  && perl -i -pe 's/check: all.*$/check: all/' qt/chardict/po/Makefile.in.in \
-  && ed qt/chardict/po/Makefile.in.in <<EOT
-/^check:
-+
-.,/^$/d
-i
-# The test is removed for qt/chardict/po  -- YamaKen
-
-.
-wq
-EOT
+  && generate_makefile_in_in qt/chardict/po \
+  && generate_makefile_in_in qt4/chardict/po
 
 # Since intltool 0.32 and later does not include po/Makevars into Makefile by
 # default as we expected, I do the '# Makevars' workaround here. See
