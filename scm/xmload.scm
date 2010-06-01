0d04f651a276bee092474cde244292f0167fe097
ek.kato <ek.kato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2010-06-01 04:51:14 +0000

    - Add wubi input methods [uim-en 323].
    * scm/Makefile.am
      - (SCM_FILES) : Add xmload.scm wb86.scm zm.scm.
      - (module_names) : Add xmload.
    * scm/zm.scm : New table.  ZhengMa input method by 余钰炜.
    * scm/wb86.scm : New table.  WuBi86 input method by 余钰炜.
    * xmload.scm : New.  Load zm.scm and wb86.scm.

---
+++ scm/xmload.scm
@@ -0,0 +1,57 @@
+;;;
+;;; Copyright (c) 2010 uim Project http://code.google.com/p/uim/
+;;;
+;;; All rights reserved.
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions
+;;; are met:
+;;; 1. Redistributions of source code must retain the above copyright
+;;;    notice, this list of conditions and the following disclaimer.
+;;; 2. Redistributions in binary form must reproduce the above copyright
+;;;    notice, this list of conditions and the following disclaimer in the
+;;;    documentation and/or other materials provided with the distribution.
+;;; 3. Neither the name of authors nor the names of its contributors
+;;;    may be used to endorse or promote products derived from this software
+;;;    without specific prior written permission.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+;;; ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+;;; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+;;; OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;;; HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+;;; LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+;;; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+;;; SUCH DAMAGE.
+;;;;
+
+(require "generic.scm")
+;;
+(define zm-init-handler
+  (lambda (id im arg)
+    (require "zm.scm")
+    (generic-context-new id im zm-rule #f)))
+
+(generic-register-im
+ 'zm
+ "zh_CN:zh_TW:zh_HK"
+ "UTF-8"
+ (N_ "ZhengMa")
+ (N_ "ZhengMa input method (Chinese)")
+ zm-init-handler)
+
+(define wb86-init-handler
+  (lambda (id im arg)
+    (require "wb86.scm")
+    (generic-context-new id im wb86-rule #f)))
+
+(generic-register-im
+ 'wb86
+ "zh_CN:zh_TW:zh_HK"
+ "UTF-8"
+ (N_ "WuBi 86")
+ (N_ "WuBi 86 input method (Chinese)")
+ wb86-init-handler)
