e09439388492beb2501a900ca8d6b4f97e87bc06
ek.kato <ek.kato@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2010-07-28 06:25:12 +0000

    * make-dist.sh : Update.

--- make-dist.sh
+++ make-dist.sh
@@ -7,17 +7,21 @@ SSCM_REPOSITORY="${UIM_REPOSITORY}/sigscheme-trunk"
 LIBGCROOTS_REPOSITORY="${UIM_REPOSITORY}/libgcroots-trunk"
 TAGS_REPOSITORY="${UIM_REPOSITORY}/tags"
 #SSCM_URL="${SSCM_REPOSITORY}"
-SSCM_URL="${TAGS_REPOSITORY}/sigscheme-0.8.4"
+SSCM_URL="${TAGS_REPOSITORY}/sigscheme-0.8.5"
 #LIBGCROOTS_URL="${LIBGCROOTS_REPOSITORY}"
 LIBGCROOTS_URL="${TAGS_REPOSITORY}/libgcroots-0.2.2"
 RELEASE_SUFFIX=""
 
-CONF_COMMON="--enable-maintainer-mode --disable-warnings-into-error"
+CONF_MAINT="--enable-maintainer-mode"
+CONF_NOWERROR="--disable-warnings-into-error"
+CONF_COMMON="$CONF_MAINT $CONF_NOWERROR"
 CONF_NONE="$CONF_COMMON --disable-debug --disable-fep --disable-emacs --disable-gnome-applet --disable-kde-applet --disable-pref --disable-dict --without-anthy --without-canna --without-mana --without-prime --without-m17nlib --without-scim --without-gtk2 --without-gnome2 --without-qt --without-qt-immodule --disable-compat-scm --without-eb --without-libedit"
 CONF_DEFAULT="$CONF_COMMON"
 # --without-scim since it is broken
+# --without-qt
 # FIXME: hardcoded --with-eb-conf
-CONF_FULL="$CONF_COMMON --enable-debug --enable-fep --enable-emacs --enable-gnome-applet --enable-kde-applet --enable-pref --enable-dict --enable-notify --with-anthy --with-canna --with-wnn --with-sj3 --with-mana --with-prime --with-m17nlib --without-scim --with-gtk2 --with-gnome2 --with-qt --with-qt-immodule --enable-compat-scm --with-eb --with-eb-conf=/etc/eb.conf --with-libedit"
+CONF_FULL_WO_MAINT="$CONF_NOWERROR --enable-debug --enable-fep --enable-emacs --enable-gnome-applet --enable-kde-applet --enable-pref --enable-dict --enable-notify --with-anthy --with-canna --with-wnn --with-sj3 --with-mana --with-prime --with-m17nlib --without-scim --with-gtk2 --with-gnome2 --without-qt --without-qt-immodule --enable-compat-scm --with-eb --with-eb-conf=/etc/eb.conf --with-libedit"
+CONF_FULL="$CONF_MAINT $CONF_FULL_WO_MAINT"
 
 
 svn export $SSCM_URL sigscheme
@@ -40,7 +44,16 @@ wq
 EOT
 fi
 
-for conf_args in "$CONF_NONE" "$CONF_DEFAULT" "$CONF_FULL"; do
+for conf_args in "$CONF_NONE" "$CONF_DEFAULT"; do
+    echo "configure $conf_args"
+    ./configure $conf_args && $MAKE dist \
+      || { echo 'make dist failed'; exit 1; }
+done
+
+# N.B. make check in test/ requires debug, nls and most of IMs enabled
+# Also do make check without --enable-maintainer-mode
+export DISTCHECK_CONFIGURE_FLAGS="$CONF_FULL_WO_MAINT"
+for conf_args in "$CONF_FULL"; do
     echo "configure $conf_args"
     ./configure $conf_args && $MAKE distcheck sum \
       || { echo 'make distcheck failed'; exit 1; }
