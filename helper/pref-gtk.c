cfbd0ba656295a174e748abc843b6de9ad0f708b
deton.kih <deton.kih@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2010-08-14 23:49:23 +0000

    * helper/pref-gtk.c   - (check_dot_uim_file): Suppress ~/.uim check and warning dialog   if uim-pref-suppress-dot-uim-warning-dialog? is #t.

--- helper/pref-gtk.c
+++ helper/pref-gtk.c
@@ -43,6 +43,7 @@
 
 #include "uim/uim.h"
 #include "uim/uim-custom.h"
+#include "uim/uim-scm.h"
 #include "uim/gettext.h"
 #include "pref-gtk-custom-widgets.h"
 #include "../gtk/key-util-gtk.h"
@@ -567,14 +568,23 @@ create_pref_window(void)
 static gboolean
 check_dot_uim_file(void)
 {
-  GString *dot_uim = g_string_new(g_get_home_dir());
+  GString *dot_uim;
   GtkWidget *dialog;
   const gchar *message =
     N_("The user customize file \"~/.uim\" is found.\n"
        "This file will override all conflicted settings set by\n"
        "this tool (stored in ~/.uim.d/customs/*.scm).\n"
-       "Please check the file if you find your settings aren't applied.");
+       "Please check the file if you find your settings aren't applied.\n\n"
+       "(To suppress this dialog, add following line to ~/.uim)\n"
+       "(define uim-pref-suppress-dot-uim-warning-dialog? #t)");
+  uim_bool suppress_dialog;
 
+  suppress_dialog = uim_scm_symbol_value_bool("uim-pref-suppress-dot-uim-warning-dialog?");
+  if (suppress_dialog) {
+    return FALSE;
+  }
+
+  dot_uim = g_string_new(g_get_home_dir());
   g_string_append(dot_uim, "/.uim");
 
   if (!g_file_test(dot_uim->str, G_FILE_TEST_EXISTS)) {
@@ -592,12 +602,6 @@ check_dot_uim_file(void)
     gtk_window_set_transient_for(GTK_WINDOW(dialog), GTK_WINDOW(pref_window));
     gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER_ON_PARENT);
   }
-
-  /*
-   *  2005-02-07 Takuro Ashie <ashie@homa.ne.jp>
-   *    FIXME! We shoud add a check box like
-   *    "Show this message every time on start up.".
-   */
 
   gtk_dialog_run(GTK_DIALOG(dialog));
   gtk_widget_destroy(GTK_WIDGET(dialog));
