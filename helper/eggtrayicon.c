095f4bbc967e4f058c80a499f0bdd1e16a6c276e
nogu.dev <nogu.dev@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2011-02-17 11:58:00 +0000

    * helper/eggtrayicon.c   - (egg_tray_icon_draw): Fix coordinates in GTK+3.

--- helper/eggtrayicon.c
+++ helper/eggtrayicon.c
@@ -315,6 +315,18 @@ egg_tray_icon_expose (GtkWidget *widget,
 #if GTK_CHECK_VERSION(2, 18, 0)
   if (focus_child && gtk_widget_has_focus (focus_child))
     {
+#if GTK_CHECK_VERSION(2, 90, 0)
+      border_width = gtk_container_get_border_width(GTK_CONTAINER (widget));
+
+      x = border_width;
+      y = border_width;
+
+      width  = gtk_widget_get_allocated_width (widget);
+      height = gtk_widget_get_allocated_height (widget);
+
+      gtk_render_focus (gtk_widget_get_style_context(widget),
+                        cr, x, y, width, height);
+#else
       GtkAllocation allocation;
       border_width = gtk_container_get_border_width(GTK_CONTAINER (widget));
 
@@ -326,10 +338,6 @@ egg_tray_icon_expose (GtkWidget *widget,
       width  = allocation.width  - 2 * border_width;
       height = allocation.height - 2 * border_width;
 
-#if GTK_CHECK_VERSION(2, 90, 0)
-      gtk_render_focus (gtk_widget_get_style_context(widget),
-                        cr, x, y, width, height);
-#else
       gtk_paint_focus (gtk_widget_get_style(widget),
                        gtk_widget_get_window(widget),
                        gtk_widget_get_state (widget),
