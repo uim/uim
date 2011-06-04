260bb8dc0e54e1c6e16292d672e73d81575d01e7
nogu.dev <nogu.dev@ff9ab193-ed19-0410-b3e9-fd3dc8ba15a0>
2011-06-04 15:18:17 +0000

    * Merge r7100, r7101, r7105 from trunk.

--- make-dist.sh
+++ make-dist.sh
@@ -17,12 +17,24 @@ CONF_NOWERROR="--disable-warnings-into-error"
 CONF_COMMON="$CONF_MAINT $CONF_NOWERROR"
 CONF_NONE="$CONF_COMMON --disable-debug --disable-fep --disable-emacs --disable-gnome-applet --disable-kde-applet --disable-pref --disable-dict --without-anthy --without-canna --without-mana --without-prime --without-skk --without-x --without-xft --without-m17nlib --without-scim --without-gtk2 --without-qt --without-qt-immodule --disable-compat-scm --without-eb --without-libedit"
 CONF_DEFAULT="$CONF_COMMON"
+
+for file in "/etc/eb.conf" "/usr/lib64/eb.conf" "/usr/lib/eb.conf" \
+            "/usr/local/etc/eb.conf" "/usr/etc/eb.conf"
+do
+    if test -f "$file"; then
+        EB_CONF="$file"
+        break
+    fi
+done
+if test -z "$EB_CONF"; then
+    echo eb.conf not found
+    exit 1
+fi
+
 # --without-scim since it is broken
 # --without-qt
-# FIXME: hardcoded --with-eb-conf
-CONF_FULL_WO_MAINT="$CONF_NOWERROR --enable-debug --enable-fep --enable-emacs --enable-gnome-applet --enable-gnome3-applet --enable-kde-applet --enable-kde4-applet --enable-pref --enable-dict --enable-notify --with-anthy --with-canna --with-wnn --with-sj3 --with-mana --with-prime --with-m17nlib --without-scim --with-gtk2 --with-gtk3 --without-qt --without-qt-immodule --enable-compat-scm --with-eb --with-eb-conf=/etc/eb.conf --with-libedit --with-qt4 --with-qt4-immodule"
+CONF_FULL_WO_MAINT="$CONF_NOWERROR --enable-debug --enable-fep --enable-emacs --enable-gnome-applet --enable-gnome3-applet --enable-kde-applet --enable-kde4-applet --enable-pref --enable-dict --enable-notify --with-anthy --with-canna --with-wnn --with-sj3 --with-mana --with-prime --with-m17nlib --without-scim --with-gtk2 --with-gtk3 --without-qt --without-qt-immodule --enable-compat-scm --with-eb --with-eb-conf=$EB_CONF --with-libedit --with-qt4 --with-qt4-immodule"
 CONF_FULL="$CONF_MAINT $CONF_FULL_WO_MAINT"
-
 
 svn export $SSCM_URL sigscheme
 svn export $LIBGCROOTS_URL sigscheme/libgcroots
