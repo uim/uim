uim/uim

commit ee8d42f1bace0dd213789dfd2397fd5bf549d5c1

Add support for socket to AbstractCandidateWindow.
* qt4/candwin/abstractcandidatewindow.cpp
  - (AbstractCandidateWindow::AbstractCandidateWindow):
    Add a socket notifier.
  - (AbstractCandidateWindow::slotStdinActivated): New.
* qt4/candwin/abstractcandidatewindow.h
  - (AbstractCandidateWindow::slotStdinActivated): New.
* qt4/candwin/qt4.cpp
  - (main)
    * Support CandidateWindow and CandidateTableWindow.
    * call uim_init() to prevent crash.
* qt4/immodule/candidatewindowproxy.cpp
  - (CandidateWindowProxy::isVisible): Change return value.
  - (CandidateWindowProxy::deactivateCandwin,
     CandidateWindowProxy::clearCandidates,
     CandidateWindowProxy::popup,
     CandidateWindowProxy::layoutWindow,
     CandidateWindowProxy::candidateActivate,
     CandidateWindowProxy::candidateActivateWithDelay,
     CandidateWindowProxy::candidateSelect,
     CandidateWindowProxy::candidateShiftPage):
    Change message.
  - (CandidateWindowProxy::initializeProcess): Add "-v" argument.
  - (CandidateWindowProxy::slotReadyStandardOutput): Implement.
* qt4/immodule/quiminputcontextplugin.pro.in
  - (INCLUDEPATH): Add candwin.
  - (HEADERS): Add util.h.
  - (SOURCES): Add uitl.cpp.

qt4/candwin/qt4.cpp
@@ -30,9 +30,11 @@
  SUCH DAMAGE.
 
 */
-#include "ximcandidatewindow.h"
-
 #include <QtGui/QApplication>
+
+#include "candidatewindow.h"
+#include "candidatetablewindow.h"
+#include "ximcandidatewindow.h"
 
 #include "qtgettext.h"
 
@@ -43,9 +45,28 @@ int main(int argc, char *argv[])
     textdomain(PACKAGE);
     bind_textdomain_codeset(PACKAGE, "UTF-8"); // ensure code encoding is UTF8-
     
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    uim_init();
+    if (argc > 1) {
+        // vertical
+        if (!strcmp(argv[1], "-v")) {
+            CandidateWindow window(0);
+            window.show();
+        // horizontal
+        } else if (!strcmp(argv[1], "-h")) {
+            CandidateWindow window(0, false);
+            window.show();
+        // table
+        } else if (!strcmp(argv[1], "-t")) {
+            CandidateTableWindow window(0);
+            window.show();
+        } else {
+            XimCandidateWindow window;
+        }
+    } else {
+        XimCandidateWindow window;
+    }
+    uim_quit();
 
-    XimCandidateWindow b;
-
-    return a.exec();
+    return app.exec();
 }

qt4/candwin/abstractcandidatewindow.cpp
@@ -32,11 +32,14 @@
 */
 #include "abstractcandidatewindow.h"
 
+#include <QtCore/QSocketNotifier>
 #include <QtCore/QTimer>
 #include <QtGui/QApplication>
 #include <QtGui/QDesktopWidget>
 #include <QtGui/QLabel>
 #include <QtGui/QMoveEvent>
+
+#include "util.h"
 
 const Qt::WindowFlags candidateFlag = (Qt::Window
                                         | Qt::WindowStaysOnTopHint
@@ -62,6 +65,10 @@ AbstractCandidateWindow::AbstractCandidateWindow(QWidget *parent)
     m_delayTimer->setSingleShot(true);
     connect(m_delayTimer, SIGNAL(timeout()), this, SLOT(timerDone()));
 #endif /* !UIM_QT_USE_DELAY */
+
+    notifier = new QSocketNotifier(0, QSocketNotifier::Read);
+    connect(notifier, SIGNAL(activated(int)),
+        this, SLOT(slotStdinActivated(int)));
 }
 
 AbstractCandidateWindow::~AbstractCandidateWindow()
@@ -478,4 +485,40 @@ bool AbstractCandidateWindow::eventFilter(QObject *obj, QEvent *event)
         return false;
     }
     return QFrame::eventFilter(obj, event);
+}
+
+void AbstractCandidateWindow::slotStdinActivated(int fd)
+{
+    QList<QStringList> messageList = parse_messages(get_messages(fd));
+    for (int i = 0, j = messageList.count(); i < j; i++) {
+        QStringList message = messageList[i];
+        QString command = message[0];
+        fprintf(stdout, "%s", command.toStdString().c_str());
+        fflush(stdout);
+        if (command == "deactivate_candwin")
+            deactivateCandwin();
+        else if (command == "clear_candidates")
+            clearCandidates();
+        else if (command == "popup")
+            popup();
+        else if (command == "hide")
+            hide();
+        else if (command == "layout_window") {
+            QPoint point;
+            point.setX(message[1].toInt());
+            point.setY(message[2].toInt());
+            QRect rect;
+            rect.setHeight(message[3].toInt());
+            layoutWindow(point, rect);
+        } else if (command == "candidate_activate")
+            candidateActivate(message[1].toInt(), message[2].toInt());
+#ifdef UIM_QT_USE_DELAY
+        else if (command == "candidate_activate_with_delay")
+            candidateActivateWithDelay(message[1].toInt());
+#endif /* !UIM_QT_USE_DELAY */
+        else if (command == "candidate_select")
+            candidateSelect(message[1].toInt());
+        else if (command == "candidate_shift_page")
+            candidateShiftPage(message[1].toInt());
+    }
 }

qt4/candwin/abstractcandidatewindow.h
@@ -44,6 +44,7 @@
 #define UIM_QT_USE_DELAY 1
 
 class QLabel;
+class QSocketNotifier;
 class QTimer;
 
 class QUimInputContext;
@@ -101,10 +102,11 @@ class AbstractCandidateWindow : public QFrame
         QList<bool> pageFilled;
 #endif
 
-#ifdef UIM_QT_USE_DELAY
     private slots:
+#ifdef UIM_QT_USE_DELAY
         void timerDone();
 #endif /* !UIM_QT_USE_DELAY */
+        void slotStdinActivated(int fd);
 
     private:
         void setCandidates(int displayLimit,
@@ -131,6 +133,8 @@ class AbstractCandidateWindow : public QFrame
         // timer for delay API
         QTimer *m_delayTimer;
 #endif /* !UIM_QT_USE_DELAY */
+       
+        QSocketNotifier *notifier;
 };
 
 #endif /* Not def: UIM_QT4_IMMODULE_ABSTRACT_CANDIDATE_WINDOW_H */

qt4/immodule/candidatewindowproxy.cpp
@@ -36,6 +36,7 @@
 #include <QtCore/QProcess>
 
 #include "quiminputcontext.h"
+#include "util.h"
 
 CandidateWindowProxy::CandidateWindowProxy()
 {
@@ -52,16 +53,17 @@ CandidateWindowProxy::~CandidateWindowProxy()
 
 void CandidateWindowProxy::deactivateCandwin()
 {
-    execute("deactivate");
+    execute("deactivate_candwin");
 }
 
 void CandidateWindowProxy::clearCandidates()
 {
+    execute("clear_candidates");
 }
 
 void CandidateWindowProxy::popup()
 {
-    execute("show");
+    execute("popup");
 }
 
 void CandidateWindowProxy::hide()
@@ -71,40 +73,36 @@ void CandidateWindowProxy::hide()
 
 bool CandidateWindowProxy::isVisible()
 {
-    return true;
+    return false;
 }
 
 void CandidateWindowProxy::layoutWindow(const QPoint &point, const QRect &rect)
 {
-    Q_UNUSED(rect)
-    execute("move\f" + QString::number(point.x()) + "\f"
-        + QString::number(point.y()));
+    execute("layout_window\f" + QString::number(point.x()) + "\f"
+        + QString::number(point.y()) + "\f" + QString::number(rect.height()));
 }
 
 void CandidateWindowProxy::candidateActivate(int nr, int displayLimit)
 {
-    Q_UNUSED(nr)
-    execute("activate\fcharset=UTF-8\fdisplay_limit="
-        + QString::number(displayLimit)
-        + "\fhead1\acand1\aannot1\fhead2\acand2\aannot2\fhead3\acand3\aannot3");
+    execute("candidate_activate\f" + QString::number(nr) + "\f"
+        + QString::number(displayLimit));
 }
 
 #ifdef UIM_QT_USE_DELAY
-void candidateActivateWithDelay(int delay)
+void CandidateWindowProxy::candidateActivateWithDelay(int delay)
 {
-    Q_UNUSED(delay)
+    execute("candidate_activate_with_delay\f" + QString::number(delay));
 }
 #endif /* !UIM_QT_USE_DELAY */
 
 void CandidateWindowProxy::candidateSelect(int index)
 {
-    execute("set_nr_candidates\f" + QString::number(index) + "\f20");
+    execute("candidate_select\f" + QString::number(index));
 }
 
 void CandidateWindowProxy::candidateShiftPage(bool forward)
 {
-    Q_UNUSED(forward)
-    execute("show_page\f0");
+    execute("candidate_shift_page\f" + QString::number(forward));
 }
 
 void CandidateWindowProxy::initializeProcess()
@@ -113,7 +111,7 @@ void CandidateWindowProxy::initializeProcess()
         return;
     }
     process->close();
-    process->start("/usr/libexec/uim-candwin-qt4", QStringList());
+    process->start("/usr/libexec/uim-candwin-qt4", QStringList() << "-v");
     process->waitForStarted();
 }
 
@@ -127,17 +125,22 @@ void CandidateWindowProxy::slotReadyStandardOutput()
 {
     QByteArray output = process->readAllStandardOutput();
     qDebug("%s", output.constData());
-    if (output == "set_candwin_active\f\f") {
-        ic->setCandwinActive();
-    } else if (output.startsWith("set_candidate_index\f")) {
-        int candidateIndex = 1;
-        if (ic && ic->uimContext() && candidateIndex != -1)
-            uim_set_candidate_index(ic->uimContext(), candidateIndex);
-    } else if (output.startsWith("delay_activating\f")) {
-        int nr = -1;
-        int display_limit = -1;
-        int selected_index = -1;
-        uim_delay_activating(ic->uimContext(), &nr, &display_limit,
-            &selected_index);
+    QList<QStringList> messageList = parse_messages(QString(output));
+    for (int i = 0, j = messageList.count(); i < j; i++) {
+        QStringList message = messageList[i];
+        QString command = message[0];
+        if (command == "set_candwin_active") {
+            ic->setCandwinActive();
+        } else if (command == "set_candidate_index") {
+            int candidateIndex = message[1].toInt();
+            if (ic && ic->uimContext() && candidateIndex != -1)
+                uim_set_candidate_index(ic->uimContext(), candidateIndex);
+        } else if (command == "delay_activating") {
+            int nr = message[1].toInt();
+            int display_limit = message[2].toInt();
+            int selected_index = message[3].toInt();
+            uim_delay_activating(ic->uimContext(), &nr, &display_limit,
+                &selected_index);
+        }
     }
 }

qt4/immodule/quiminputcontextplugin.pro.in
@@ -3,6 +3,9 @@ include(../common.pro)
 TEMPLATE = lib
 CONFIG += plugin
 
+# to include util.h
+INCLUDEPATH += @srcdir@/../candwin
+
 LIBS += @X11_LIBS@ -luim-counted-init
 
 # Input
@@ -12,7 +15,8 @@ HEADERS += @srcdir@/candidatewindowproxy.h \
            @srcdir@/qhelpermanager.h \
            @srcdir@/qtextutil.h \
            @srcdir@/quiminfomanager.h \
-           @srcdir@/quiminputcontext.h
+           @srcdir@/quiminputcontext.h \
+           @srcdir@/../candwin/util.h
 
 SOURCES += @srcdir@/candidatewindowproxy.cpp \
            @srcdir@/caretstateindicator.cpp \
@@ -20,7 +24,8 @@ SOURCES += @srcdir@/candidatewindowproxy.cpp \
            @srcdir@/qhelpermanager.cpp \
            @srcdir@/qtextutil.cpp \
            @srcdir@/quiminfomanager.cpp \
-           @srcdir@/quiminputcontext.cpp
+           @srcdir@/quiminputcontext.cpp \
+           @srcdir@/../candwin/util.cpp
 
 !win32:!embedded:!mac {
     HEADERS += @srcdir@/quiminputcontext_compose.h

commit 3d027efb24e49d00522ae7c082a676c4125a795a

Fix bug introduced in ee8d42f1bace0dd213789dfd2397fd5bf549d5c1.
* qt4/candwin/qt4.cpp
  - (Window): New class to wrap candidate window classes.
  - (main): Use Window class.

qt4/candwin/qt4.cpp
@@ -38,6 +38,43 @@
 
 #include "qtgettext.h"
 
+class Window
+{
+    public:
+        Window(int argc, char *argv[]);
+        ~Window();
+
+    private:
+        QWidget *widget;
+};
+
+Window::Window(int argc, char *argv[])
+{
+    uim_init();
+    if (argc > 1) {
+        // vertical
+        if (!strcmp(argv[1], "-v")) {
+            widget = new CandidateWindow(0, true);
+        // horizontal
+        } else if (!strcmp(argv[1], "-h")) {
+            widget = new CandidateWindow(0, false);
+        // table
+        } else if (!strcmp(argv[1], "-t")) {
+            widget = new CandidateTableWindow(0);
+        } else {
+            widget = new XimCandidateWindow;
+        }
+    } else {
+        widget = new XimCandidateWindow;
+    }
+}
+
+Window::~Window()
+{
+    delete widget;
+    uim_quit();
+}
+
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "");
@@ -46,27 +83,8 @@ int main(int argc, char *argv[])
     bind_textdomain_codeset(PACKAGE, "UTF-8"); // ensure code encoding is UTF8-
     
     QApplication app(argc, argv);
-    uim_init();
-    if (argc > 1) {
-        // vertical
-        if (!strcmp(argv[1], "-v")) {
-            CandidateWindow window(0);
-            window.show();
-        // horizontal
-        } else if (!strcmp(argv[1], "-h")) {
-            CandidateWindow window(0, false);
-            window.show();
-        // table
-        } else if (!strcmp(argv[1], "-t")) {
-            CandidateTableWindow window(0);
-            window.show();
-        } else {
-            XimCandidateWindow window;
-        }
-    } else {
-        XimCandidateWindow window;
-    }
-    uim_quit();
+
+    Window window(argc, argv);
 
     return app.exec();
 }