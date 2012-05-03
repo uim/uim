37a56eefa106ebea5b48e426cf4f1024a3673291
Muneyuki Noguchi <nogu.dev@gmail.com>
2012-05-03 23:08:02 +0900

    Begin to implement Qt5 immodule.
    * qt5/immodule/quimplatforminputcontext.cpp
    * qt5/immodule/quimplatforminputcontext.h
      - Port codes from Qt4 immodule.

--- qt5/immodule/quimplatforminputcontext.h
+++ qt5/immodule/quimplatforminputcontext.h
@@ -34,11 +34,24 @@
 #ifndef UIM_QT5_IMMODULE_QUIMPLATFORMINPUTCONTEXT_H
 #define UIM_QT5_IMMODULE_QUIMPLATFORMINPUTCONTEXT_H
 
+#include <QtGui/QInputMethodEvent>
 #include <QtGui/QPlatformInputContext>
 
 #include <uim.h>
 
+class CandidateWindowProxy;
 class QUimTextUtil;
+class QUimHelperManager;
+
+struct PreeditSegment
+{
+    PreeditSegment(int attr, const QString &str) {
+        this->attr = attr;
+        this->str = str;
+    }
+    int attr;
+    QString str;
+};
 
 class QUimPlatformInputContext : public QPlatformInputContext
 {
@@ -70,7 +83,7 @@ public:
     void updatePosition();
     void updateStyle();
 
-    QUimTextUtil *textUtil() { return mTextUtil; }
+    QUimTextUtil *textUtil() { return m_textUtil; }
 
     QString getPreeditString();
     int getPreeditCursorPosition();
@@ -83,9 +96,44 @@ public:
     void setCandwinActive() { candwinIsActive = true; }
 
 private:
-    QUimTextUtil *mTextUtil;
+    uim_context createUimContext(const char *imname);
+
+    QList<QInputMethodEvent::Attribute> getPreeditAttrs();
+
+    void update();
+
+    /// callbacks for uim
+    static void commit_cb(void *ptr, const char *str);
+    // preedit
+    static void clear_cb(void *ptr);
+    static void pushback_cb(void *ptr, int attr, const char *str);
+    static void update_cb(void *ptr);
+    // candidate
+    static void cand_activate_cb(void *ptr, int nr, int displayLimit);
+    static void cand_select_cb(void *ptr, int index);
+    static void cand_shift_page_cb(void* ptr, int index);
+    static void cand_deactivate_cb(void *ptr);
+    // imsw
+    static void switch_app_global_im_cb(void *ptr, const char *str);
+    static void switch_system_global_im_cb(void *ptr, const char *str);
+    // delay
+    static void cand_activate_with_delay_cb(void *ptr, int delay);
+    /// real functions for callbacks (correspond order)
+    // preedit
+    void clearPreedit();
+    void pushbackPreeditString(int attr, const QString& str);
+    void updatePreedit();
+    // imsw
+    void switch_app_global_im(const char *str);
+    void switch_system_global_im(const char *str);
+
+    QUimTextUtil *m_textUtil;
     bool candwinIsActive;
+    bool m_isComposing;
+
     uim_context m_uc;
+    QList<PreeditSegment> preeditSegments;
+    CandidateWindowProxy *proxy;
 };
 
 #endif /* Not def: UIM_QT5_IMMODULE_QUIMPLATFORMINPUTCONTEXT_H */
