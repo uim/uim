c40947581c5203ad1ccd0d323647b50a45ca9e26
Dmitry Shachnev <mitya57@gmail.com>
2014-11-21 14:58:34 +0300

    Port to GAction and libpanel-applet 3.14.
    References:
    https://wiki.gnome.org/HowDoI/GAction
    https://wiki.gnome.org/HowDoI/ApplicationMenu
    https://mail.gnome.org/archives/gnome-announce-list/2014-October/msg00036.html

--- gtk3/toolbar/applet-gnome3.c
+++ gtk3/toolbar/applet-gnome3.c
@@ -44,83 +44,66 @@
 
 PanelApplet *uimapplet;
 
-static void exec_switcher(GtkAction *action, gpointer data);
-static void exec_pref(GtkAction *action, gpointer data);
-static void exec_dic(GtkAction *action, gpointer data);
-static void exec_pad(GtkAction *action, gpointer data);
-static void exec_hand(GtkAction *action, gpointer data);
-static void exec_help(GtkAction *action, gpointer data);
-static void display_about_dialog(GtkAction *action, gpointer data);
+static void exec_switcher(GSimpleAction *action, GVariant *parameter, gpointer data);
+static void exec_pref(GSimpleAction *action, GVariant *parameter, gpointer data);
+static void exec_dic(GSimpleAction *action, GVariant *parameter, gpointer data);
+static void exec_pad(GSimpleAction *action, GVariant *parameter, gpointer data);
+static void exec_hand(GSimpleAction *action, GVariant *parameter, gpointer data);
+static void exec_help(GSimpleAction *action, GVariant *parameter, gpointer data);
+static void display_about_dialog(GSimpleAction *action, GVariant *parameter, gpointer data);
 
 extern GtkWidget *uim_toolbar_applet_new(void);
 extern void uim_toolbar_launch_helper_application(const char *command);
 
-
-static const GtkActionEntry uim_menu_actions[] = {
-  {"Switcher", "im_switcher",
-    N_("Switch input method"), NULL, NULL, G_CALLBACK(exec_switcher)},
-  {"Pref", GTK_STOCK_PREFERENCES,
-    N_("Preference"), NULL, NULL, G_CALLBACK(exec_pref)},
-  {"Dic", "uim-dict",
-    N_("Japanese dictionary editor"), NULL, NULL, G_CALLBACK(exec_dic)},
-  {"Pad", GTK_STOCK_BOLD,
-    N_("Input pad"), NULL, NULL, G_CALLBACK(exec_pad)},
-  {"Hand", GTK_STOCK_EDIT,
-    N_("Handwriting input pad"), NULL, NULL, G_CALLBACK(exec_hand)},
-  {"Help", GTK_STOCK_HELP,
-    N_("Help"), NULL, NULL, G_CALLBACK(exec_help)},
-  {"About", GTK_STOCK_ABOUT,
-    N_("About"), NULL, NULL, G_CALLBACK(display_about_dialog)}
+static const GActionEntry uim_menu_actions[] = {
+  { "switcher", exec_switcher },
+  { "pref", exec_pref },
+  { "dic", exec_dic },
+  { "pad", exec_pad },
+  { "hand", exec_hand },
+  { "help", exec_help },
+  { "about", display_about_dialog }
 };
 
-static const char uim_menu_xml[] =
-  "<menuitem action=\"Switcher\"/>"
-  "<menuitem action=\"Pref\"/>"
-  "<menuitem action=\"Dic\"/>"
-  "<menuitem action=\"Pad\"/>"
-  "<menuitem action=\"Hand\"/>"
-  "<menuitem action=\"Help\"/>"
-  "<menuitem action=\"About\"/>";
-
 static void
-exec_switcher(GtkAction *action, gpointer data)
+exec_switcher(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
   uim_toolbar_launch_helper_application("uim-im-switcher-gtk3");
 }
 
 static void
-exec_pref(GtkAction *action, gpointer data)
+exec_pref(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
   uim_toolbar_launch_helper_application("uim-pref-gtk3");
 }
 
 static void
-exec_dic(GtkAction *action, gpointer data)
+exec_dic(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
   uim_toolbar_launch_helper_application("uim-dict-gtk3");
 }
 
 static void
-exec_pad(GtkAction *action, gpointer data)
+exec_pad(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
   uim_toolbar_launch_helper_application("uim-input-pad-ja-gtk3");
 }
 
 static void
-exec_hand(GtkAction *action, gpointer data)
+exec_hand(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
   uim_toolbar_launch_helper_application("uim-tomoe-gtk");
 }
 
 static void
-exec_help(GtkAction *uic, gpointer data)
+exec_help(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
   uim_toolbar_launch_helper_application("uim-help");
 }
 
 /* Just the about window... If it's already open, just focus it */
 static void
-display_about_dialog(GtkAction *action, gpointer data)
+display_about_dialog(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
   GdkPixbuf *icon = NULL;
   const gchar *authors[] = {"uim Project", NULL};
@@ -149,7 +132,8 @@ static gboolean
 uim_applet_new(PanelApplet *applet, const gchar *iid, gpointer data)
 {
   GtkWidget *toolbar;
-  GtkActionGroup *action_group;
+  GSimpleActionGroup *action_group;
+  gchar *ui_path;
   
   uimapplet = applet;
 
@@ -164,11 +148,16 @@ uim_applet_new(PanelApplet *applet, const gchar *iid, gpointer data)
 
   gtk_widget_show_all(GTK_WIDGET(applet));
 
-  action_group = gtk_action_group_new("uim Applet Actions");
-  gtk_action_group_set_translation_domain(action_group, GETTEXT_PACKAGE);
-  gtk_action_group_add_actions(action_group, uim_menu_actions,
+  action_group = g_simple_action_group_new();
+  g_action_map_add_action_entries(G_ACTION_MAP (action_group), uim_menu_actions,
       G_N_ELEMENTS(uim_menu_actions), toolbar);
-  panel_applet_setup_menu(applet, uim_menu_xml, action_group);
+
+  ui_path = g_build_filename (UIM_UIDATADIR, "uim-applet-menu.xml", NULL);
+  panel_applet_setup_menu_from_file(applet, ui_path, action_group, GETTEXT_PACKAGE);
+  g_free(ui_path);
+
+  gtk_widget_insert_action_group (GTK_WIDGET (applet), "uim",
+                                  G_ACTION_GROUP (action_group));
 #if LIBPANEL_APPLET_HAVE_SET_BACKGROUND_WIDGET
   panel_applet_set_background_widget(applet, GTK_WIDGET(applet));
 #endif
