ed69f3f46afded7ba6a803c3f14514832f10d613
Kouhei Sutou <kou@clear-code.com>
2018-05-10 16:42:10 +0900

    Stop to build Qt4 and Qt5 at once

--- make-dist.sh
+++ make-dist.sh
@@ -8,6 +8,15 @@ CONF_COMMON="$CONF_MAINT $CONF_NOWERROR"
 CONF_NONE="$CONF_COMMON --disable-debug --disable-fep --disable-emacs --disable-gnome-applet --disable-kde-applet --disable-pref --disable-dict --without-anthy --without-canna --without-mana --without-prime --without-skk --without-x --without-xft --without-m17nlib --without-gtk2 --without-qt --without-qt-immodule --disable-compat-scm --without-eb --without-libedit"
 CONF_DEFAULT="$CONF_COMMON"
 
+if type apt > /dev/null 2>&1; then
+    sudo apt install -V \
+	 libeb16-dev \
+	 libqt4-dev \
+	 qt5-qmake \
+	 kdelibs-bin \
+	 kdelibs5-dev
+fi
+
 for file in "/etc/eb.conf" "/usr/lib64/eb.conf" "/usr/lib/eb.conf" \
             "/usr/local/etc/eb.conf" "/usr/etc/eb.conf"
 do
@@ -21,9 +30,49 @@ if test -z "$EB_CONF"; then
     exit 1
 fi
 
-# --without-qt
-CONF_FULL_WO_MAINT="$CONF_NOWERROR --enable-debug --enable-fep --enable-emacs --enable-gnome-applet --enable-gnome3-applet --enable-kde-applet --enable-kde4-applet --enable-pref --enable-dict --enable-notify=libnotify,knotify4 --with-anthy --with-canna --with-wnn --with-sj3 --with-mana --with-prime --with-m17nlib --with-gtk2 --with-gtk3 --without-qt --without-qt-immodule --enable-compat-scm --with-eb --with-eb-conf=$EB_CONF --with-libedit --with-qt4 --with-qt4-immodule --with-qt5 --with-qt5-immodule"
-CONF_FULL="$CONF_MAINT $CONF_FULL_WO_MAINT"
+CONF_FULL_BASE_NO_MAINT="
+$CONF_NOWERROR
+--enable-debug
+--enable-fep
+--enable-emacs
+--enable-gnome-applet
+--enable-gnome3-applet
+--enable-pref
+--enable-dict
+--with-anthy
+--with-canna
+--with-wnn
+--with-sj3
+--with-mana
+--with-prime
+--with-m17nlib
+--with-gtk2
+--with-gtk3
+--without-qt
+--without-qt-immodule
+--enable-compat-scm
+--with-eb
+--with-eb-conf=$EB_CONF
+--with-libedit
+"
+CONF_FULL_QT4_WO_MAINT="
+QT_SELECT=4
+$CONF_FULL_BASE_NO_MAINT
+--enable-kde4-applet
+--enable-notify=libnotify,knotify4
+--with-qt4
+--with-qt4-immodule
+--without-qt5
+--without-qt5-immodule
+"
+CONF_FULL_QT5_WO_MAINT="
+QT_SELECT=5
+$CONF_FULL_BASE_NO_MAINT
+--enable-kde-applet
+--enable-notify=libnotify
+--with-qt5
+--with-qt5-immodule
+"
 
 git submodule update --init --recursive
 # Create a branch if a branch correspoinding to the tag doesn't exist.
@@ -33,16 +82,15 @@ git submodule update --init --recursive
  && ./autogen.sh \
 || { echo 'autogen failed.' && exit 1; }
 
-for conf_args in "$CONF_NONE" "$CONF_DEFAULT"; do
-    echo "configure $conf_args"
-    ./configure $conf_args && $MAKE dist \
-      || { echo 'make dist failed'; exit 1; }
-done
+# for conf_args in "$CONF_NONE" "$CONF_DEFAULT"; do
+#     echo "configure $conf_args"
+#     ./configure $conf_args && $MAKE dist \
+#       || { echo 'make dist failed'; exit 1; }
+# done
 
-# do make check without --enable-maintainer-mode
-export DISTCHECK_CONFIGURE_FLAGS="$CONF_FULL_WO_MAINT"
-for conf_args in "$CONF_FULL"; do
+for conf_args in "$CONF_FULL_QT4_NO_MAINT" "$CONF_FULL_QT5_NO_MAINT"; do
     echo "configure $conf_args"
-    ./configure $conf_args && $MAKE distcheck sum \
+    export DISTCHECK_CONFIGURE_FLAGS="$conf_args"
+    ./configure $CONF_MAINT $conf_args && $MAKE distcheck sum \
       || { echo 'make distcheck failed'; exit 1; }
 done
