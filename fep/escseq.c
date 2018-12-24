c2739782f550f2649b33d149b038fe299417aeda
OBATA Akio <obache@outlook.com>
2018-12-24 15:26:49 +0900

    pass 9 parameters to tparm(3) for portability
    "tparm(3) with variable argment list" is a ncuese extension,
    so 9 parameters should be passed for portability.

--- fep/escseq.c
+++ fep/escseq.c
@@ -964,7 +964,7 @@ void put_cursor_address(int row, int col)
   if (row == s_cursor.row && col == s_cursor.col && col < g_win->ws_col - 2) {
     return;
   }
-  tmp = tparm(cursor_address, row, col);
+  tmp = tparm(cursor_address, row, col, 0, 0, 0, 0, 0, 0, 0);
   my_putp(tmp);
   s_cursor.row = row;
   s_cursor.col = col;
@@ -985,7 +985,7 @@ void put_insert(int n)
   if (n <= 0) {
     return;
   }
-  tmp = tparm(parm_ich, n);
+  tmp = tparm(parm_ich, n, 0, 0, 0, 0, 0, 0, 0, 0);
   my_putp(tmp);
   debug(("<ins %d>", n));
 }
@@ -1007,7 +1007,7 @@ void put_delete(int n)
     change_background_attr(&s_attr, s_attr_uim);
   }
 
-  tmp = tparm(parm_dch, n);
+  tmp = tparm(parm_dch, n, 0, 0, 0, 0, 0, 0, 0, 0);
   my_putp(tmp);
   debug(("<del %d>", n));
 }
@@ -1036,7 +1036,7 @@ void put_goto_lastline(int col)
   if (row == s_cursor.row && col == s_cursor.col) {
     return;
   }
-  tmp = tparm(cursor_address, row, col);
+  tmp = tparm(cursor_address, row, col, 0, 0, 0, 0, 0, 0, 0);
   my_putp(tmp);
   s_cursor.row = row;
   s_cursor.col = col;
@@ -1103,7 +1103,7 @@ void put_clear_to_end_of_line(int width)
  */
 void put_change_scroll_region(int start, int end)
 {
-  const char *tmp = tparm(change_scroll_region, start, end);
+  const char *tmp = tparm(change_scroll_region, start, end, 0, 0, 0, 0, 0, 0, 0);
   my_putp(tmp);
   s_cursor.row = s_cursor.col = 0;
   debug(("<region %d %d>", start, end));
