ecd9dbf9dad96927c5662ede6f9c7bba92c412ed
YOSHINO Yoshihito <yy.y.ja.jp@gmail.com>
2024-12-07 23:24:15 +0900

    Prep for Qt 6
    Remove unused include.

--- qt4/candwin/qt4.cpp
+++ qt4/candwin/qt4.cpp
@@ -40,7 +40,6 @@
 
 #include <QtCore/QSocketNotifier>
 #include <QtCore/QStringList>
-#include <QtCore/QTextCodec>
 #if QT_VERSION < 0x050000
 # include <QtGui/QApplication>
 # include <QtGui/QDesktopWidget>
