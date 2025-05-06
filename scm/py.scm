90ae687bd21aaaf8f08f9a99662b378cf0879917
dai <1313202+dai-vdr@users.noreply.github.com>
2025-05-06 15:55:08 +0900

    replace Free Software Foundation (FSF)'s old postal address with URL. (#234)
    This is their previous address. It is pointed by Lintian
    (https://lintian.debian.org/tags/old-fsf-address-in-copyright-file.html).

--- scm/py.scm
+++ scm/py.scm
@@ -20,10 +20,8 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; Lesser General Public License for more details.
 
-;; You should have received a copy of the GNU Lesser General Public
-;; License along with the m17n library; if not, write to the Free
-;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-;; 02111-1307, USA.
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with the m17n library; if not, see <https://www.gnu.org/licenses/>.
 
 ;; The map "pinyin" was generated from the "Unihan database" belonging
 ;; to the "Unicode Character Database", and that part is governed by
